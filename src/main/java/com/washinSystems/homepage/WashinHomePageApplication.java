26f0cf35d3bd66e2648c3520c2be432fa2373280
YokoyamaAkira <akr_factory@yahoo.co.jp>
2018-03-20 15:06:35 +0900

    アプリケーション名を変更

--- src/main/java/com/washinSystems/homepage/WashinHomePageApplication.java
+++ src/main/java/com/washinSystems/homepage/WashinHomePageApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HajibootSecurityApplication {
+public class WashinHomePageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HajibootSecurityApplication.class, args);
+		SpringApplication.run(WashinHomePageApplication.class, args);
 	}
 }
