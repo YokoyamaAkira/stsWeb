42694c907f916c7d411cdba2b2470d3906dc79c7
akira <akira@akira-PC>
2018-03-25 22:25:33 +0900

    index用コントローラ追加 ビルドして動作確認

---
+++ src/main/java/com/washinSystems/homepage/web/CompanyInfoController.java
@@ -0,0 +1,17 @@
+package com.washinSystems.homepage.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CompanyInfoController {
+	@RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+	
+	@RequestMapping("/index")
+    public String index2() {
+        return "index";
+    }
+}
